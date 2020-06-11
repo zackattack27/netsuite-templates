c9683e8585152415084933b2ba09ed117b42423d
Zack Haight <zack.haight@planttherapy.com>
2020-06-11 11:28:56 -0600

    - Adding a Data Custom Module

---
+++ ss2-custom-module-data.js
@@ -0,0 +1,17 @@
+/**
+ * ss2-custom-module-data.js
+ * @NApiVersion 2.x
+ * @NModuleScope Public
+ *
+ *@Author: 
+ *@Description: 
+ */
+
+define([], callback);
+
+function callback() {
+    return {
+        Param1: "Value 1",
+        Param2: "Value 2"
+    }
+}
